6c26f3f34cd6f68b239babb558f3c70b1b4f4d86
starfish01 <patrick.labes@gmail.com>
2018-04-25 08:56:04 +1000

    fixed login button issue started work on the dashboard page

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,26 +1,34 @@
 $(function() {
+
+	$('#login-form-link').hide();
+
     $('#login-form-link').click(function(e) {
-    	$("#login-form").delay(100).fadeIn(100);
+		$("#login-form").delay(100).fadeIn(100);
+		$('#login-form-link').fadeOut(100);
+
+		
  		$("#register-form").fadeOut(100);
 		$('#register-form-link').removeClass('active');
+		$('#login-submit').show();
+
+
         $(this).addClass('active');
 
-        $('#login-form-link').fadeOut(100);
         $('#register-form-link').delay(100).fadeIn(100);
 
-        
-		e.preventDefault();
 	});
 	$('#register-form-link').click(function(e) {
-        $("#register-form").delay(100).fadeIn(100);
-        
-        $('#register-form-link').fadeOut(100);
-        $('#login-form-link').delay(100).fadeIn(100);
+		$('#login-submit').hide();
+		$("#register-form").delay(100).fadeIn(100);
+		$('#register-form-link').fadeOut(100);
 
- 		$("#login-form").fadeOut(100);
+		$("#login-form").fadeOut(100);
 		$('#login-form-link').removeClass('active');
+		
+
 		$(this).addClass('active');
-        e.preventDefault();
+
+        $('#login-form-link').delay(100).fadeIn(100);
         
 	});
 
